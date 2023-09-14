2bb7e48b5ca8f92d648811c476dcae27550c528c
Travis B. Hartwell <nafai@travishartwell.net>
2023-09-14 00:06:04 -0700

    Fix MyPy execution and implement basic test

--- datacleanup/project.sh
+++ datacleanup/project.sh
@@ -18,8 +18,20 @@ function build() {
     hatch build
 }
 
-function lint-all() {
-    hatch run lint:all
+function lint() {
+    hatch run lint:style
+}
+
+function test() {
+    hatch run default:cov
+}
+
+function test-all() {
+    hatch run default:all
+}
+
+function lint-typing() {
+    hatch run default:typing
 }
 
 function fmt() {
@@ -29,21 +41,29 @@ function fmt() {
 function install() {
     local -r wheel="${DIST_DIR}/datacleanup-0.0.1-py3-none-any.whl"
 
-    build
-
     if [[ ! -e "${wheel}" ]]; then
         echo >&2 "Missing wheel ${wheel}."
         return 1
     fi
 
+    if command -v datacleanup &>/dev/null; then
+        pipx uninstall datacleanup
+    fi
+
     pipx install --include-deps "${wheel}"
+}
+
+function all() {
+    for task in lint test-all build install; do
+        call_task "${task}"
+    done
 }
 
 function function_exists() {
     declare -F "$1" >/dev/null
 }
 
-function call_if_function_exists() {
+function call_task() {
     local -r fn=$1
     shift
 
@@ -61,4 +81,4 @@ if (($# == 0)); then
     exit 1
 fi
 
-call_if_function_exists "${@}"
+call_task "${@}"
---
+++ datacleanup/tests/test_common.py
@@ -0,0 +1,60 @@
+import csv
+from pathlib import Path
+
+import pytest
+
+from datacleanup.clean.common import write_output_file
+
+
+@pytest.fixture
+def transactions():
+    return [
+        {"date": "1/1/2020", "description": "Expense", "amount": 100},
+        {"date": "2/1/2020", "description": "Income", "amount": 200},
+    ]
+
+
+@pytest.fixture
+def output_path(tmp_path):
+    return Path(tmp_path / "output.csv")
+
+
+def test_writes_header(transactions, output_path):
+    # Act
+    write_output_file(transactions, output_path)
+
+    # Assert
+    with open(output_path) as f:
+        header = next(csv.reader(f))
+        assert header == ["date", "description", "amount"]
+
+
+def test_writes_rows(transactions, output_path):
+    # Act
+    write_output_file(transactions, output_path)
+
+    # Assert
+    with open(output_path) as f:
+        rows = list(csv.reader(f))
+        assert rows[1] == ["1/1/2020", "Expense", "100"]
+        assert rows[2] == ["2/1/2020", "Income", "200"]
+
+
+def test_empty_input(output_path):
+    # Arrange
+    transactions = []
+
+    # Act
+    write_output_file(transactions, output_path)
+
+    # Assert
+    with open(output_path) as f:
+        rows = list(csv.reader(f))
+        assert len(rows) == 1  # Only header
+
+
+def test_invalid_path(transactions):
+    output_path = Path("invalid/path")
+
+    with pytest.raises(IOError):
+        write_output_file(transactions, output_path)
